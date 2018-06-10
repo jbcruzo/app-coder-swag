5f64a91acbd1845df37426e43ec48487dab36229
Juan Bernardo Cruz Ortiz <Bernardo@macs-MacBook-Pro.local>
2018-06-09 21:57:16 -0500

    Lesson 03 - Collection Views (grid layouts)

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Juan Bernardo Cruz Ortiz on 6/9/18.
+//  Copyright © 2018 Juan Bernardo Cruz Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+}
